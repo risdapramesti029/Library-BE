93ea988c785c91e2991492a5756a1122011141e9
risdapramesti029 <2110501011@mahasiswa.upnvj.ac.id>
2025-01-24 12:32:45 +0700

    update database

--- config/database.js
+++ config/database.js
@@ -1,25 +1,11 @@
-// config/database.js
-/*const { Pool } = require('pg');
-require('dotenv').config();
-
-const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "librarydb",
-  password: "123456789",
-  port: 5432, 
-})
-module.exports = pool;
-*/
-
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'postgres',        // Ganti dengan username PostgreSQL Anda
-  host: 'localhost',       // Ganti jika host Anda berbeda
-  database: 'librarydb',   // Nama database PostgreSQL Anda
-  password: '123456789',   // Password PostgreSQL Anda
-  port: 5432,              // Port PostgreSQL (default: 5432)
+  user: 'postgres',        
+  host: 'localhost',       
+  database: 'librarydb',   
+  password: '123456789',   
+  port: 5432,              
 });
 
 module.exports = pool;
